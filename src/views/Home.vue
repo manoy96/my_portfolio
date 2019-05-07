562a84b07e9b2261bc1f9837a610c0e8126d54fa
manoy96 <mfewcar@gmail.com>
2019-05-06 22:24:12 -0600

    trying to get a fluid layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="home">
+  <div class="purple darken-2 text-xs-center">
     <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <v-container>
+      <v-layout>
+      </v-layout>
+    </v-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    //  HelloWorld
   }
 };
 </script>
